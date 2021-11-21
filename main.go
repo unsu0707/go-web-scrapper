18c308431dae507c766cebae8034f175e9cc1b46
unsu0707 <unsu0707@gmail.com>
2021-11-21 20:31:36 +0900

    scrape paginataion a's length through goquery

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://jp.indeed.com/jobs?q=python&limit=50"
+
+func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Find("a").Length())
+	})
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("request failed with status: ", res.StatusCode)
+	}
 }
