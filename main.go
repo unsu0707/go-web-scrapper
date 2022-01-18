8e3bbf3ad80190a98c383214d1e8b1338f6b18f0
unsu0707 <unsu0707@gmail.com>
2022-01-18 15:06:08 +0900

    user can download scrapped file

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/unsu0707/go-web-scrapper/scrapper"
+import (
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/unsu0707/go-web-scrapper/scrapper"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
 
 func main() {
-	scrapper.Scrape("python");
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -87,10 +87,10 @@ func getJobsFromPage(page int, url string, mainC chan<- []extractedJob) {
 
 func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
-	title := cleanString(card.Find(".jobTitle").Text())
-	location := cleanString(card.Find(".companyLocation").Text())
-	salary := cleanString(card.Find(".salary-snippet-container").Text())
-	summary := cleanString(card.Find(".job-snippet").Text())
+	title := CleanString(card.Find(".jobTitle").Text())
+	location := CleanString(card.Find(".companyLocation").Text())
+	salary := CleanString(card.Find(".salary-snippet-container").Text())
+	summary := CleanString(card.Find(".job-snippet").Text())
 	c <- extractedJob{
 		id:       id,
 		title:    title,
@@ -99,7 +99,7 @@ func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 		summary:  summary}
 }
 
-func cleanString(str string) string {
+func CleanString(str string) string {
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
